83d082512547cdcc53d37059ea86415e7eed3480
Tafita Hery <tafitahery@gmail.com>
2022-07-03 17:21:46 +0300

    Add structure in page ListReservation

--- src/pages/ListReservations/index.jsx
+++ src/pages/ListReservations/index.jsx
@@ -4,6 +4,20 @@ const ListReservations = () => {
   return (
     <div>
       <h1>Liste des réservations</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Client</th>
+            <th>Chambre</th>
+            <th>Date entrée</th>
+            <th>Date sortie</th>
+            <th>Sejour</th>
+            <th>Prix</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
     </div>
   );
 };
