63d5606b497912ceb106fdb0a7b174bdbef5cb5c
Tafita Hery <tafitahery@gmail.com>
2022-07-04 17:01:10 +0300

    Add structure in pages CreateRoom and ListRooms

--- src/pages/CreateRoom/index.jsx
+++ src/pages/CreateRoom/index.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CreateRoom = () => {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState(0);
+
   return (
     <div>
       <h1>Création d'un chambre</h1>
+      <div>
+        <div>
+          <label htmlFor="name">Nom Chambre</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="price">Prix</label>
+          <input
+            type="number"
+            id="price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
+        <div>
+          <button>Valider</button>
+          <button
+            onClick={() => {
+              setName('');
+              setPrice(0);
+            }}
+          >
+            Annuler
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/ListRooms/index.jsx
+++ src/pages/ListRooms/index.jsx
@@ -4,6 +4,15 @@ const ListRooms = () => {
   return (
     <div>
       <h1>Liste des chambres</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Nom du chambre</th>
+            <th>Prix / nuit</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
     </div>
   );
 };
