3c3d5c3a0637027ab05cfbee1e387f6a498c3d9a
Tafita Hery <tafitahery@gmail.com>
2022-07-18 14:57:15 +0300

    Add style to component Navigation with React-Bootstrap

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,30 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 
 const Navigation = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Reservation</Link>
-        </li>
-        <li>
-          <Link to="/listReservations">Liste des réservations</Link>
-        </li>
-        <li>
-          <Link to="/createClient">Création Client</Link>
-        </li>
-        <li>
-          <Link to="/listClients">Liste des clients</Link>
-        </li>
-        <li>
-          <Link to="/createRoom">Création Chambre</Link>
-        </li>
-        <li>
-          <Link to="/listRooms">Liste des chambre</Link>
-        </li>
-      </ul>
-    </div>
+    // <div>
+    //   <ul>
+    //     <li>
+    //       <Link to="/">Reservation</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/listReservations">Liste des réservations</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/createClient">Création Client</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/listClients">Liste des clients</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/createRoom">Création Chambre</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/listRooms">Liste des chambre</Link>
+    //     </li>
+    //   </ul>
+    // </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Acceuil</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Réservation
+            </Nav.Link>
+            <Nav.Link as={Link} to="/listReservations">
+              Liste des réservation
+            </Nav.Link>
+            <Nav.Link as={Link} to="/createClient">
+              Création Client
+            </Nav.Link>
+            <Nav.Link as={Link} to="/listClients">
+              Liste des client
+            </Nav.Link>
+            <Nav.Link as={Link} to="/createRoom">
+              Création Chambre
+            </Nav.Link>
+            <Nav.Link as={Link} to="/listRooms">
+              Liste des chambres
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
